df179be87c4b6ef694a42a2542dfd924893f8da2
Alex <alexseeley134@gmail.com>
2025-04-07 21:19:17 -0600

    Add image and change text colors to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,41 @@
 // app/page.tsx
 import Link from "next/link";
+import Image from "next/image";
+import Typewriter from "../public/typewriter.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Welcome to My Blog</h1>
-      <p className="mb-4">This is the homepage. Check out the blog!</p>
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <Link href="/" className="text-blue-500 hover:underline">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className="text-blue-500 hover:underline">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog" className="text-blue-500 hover:underline">
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </main>
+    <div className="w-full ">
+      <main
+        className={` bg-[url('../public/typewriter.jpg')] bg-cover flex min-h-screen flex-col items-center p-24`}
+      >
+        <h1 className="text-4xl font-bold mb-8 text-white">
+          Welcome to My Blog
+        </h1>
+        <p className="mb-4 text-white">
+          This is the homepage. Check out the blog!
+        </p>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/" className="text-blue-500 hover:underline">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="text-blue-500 hover:underline">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className="text-blue-500 hover:underline">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </main>
+      <div>More content!</div>
+    </div>
   );
 }
